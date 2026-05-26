fae919f6186e2829adf675b32640f91ebd1726d1
Om Pratap Dhaker <opdhaker2007@gmail.com>
2026-05-26 08:04:36 +0530

    lil update hehe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 const Home = () => {
   return (
     <div className="flex flex-1 items-center justify-center">
+      <h1 className="font-serif text-7xl">MoM Generator</h1>
       Happy Coding{" <3"}
     </div>
   );
